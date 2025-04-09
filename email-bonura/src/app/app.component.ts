0d876318d6ee5bf38b47e1853053bb806f9d4148
ElAl3x104 ! <bonura.alex@iisgalvanimi.edu.it>
2025-04-09 12:56:43 +0000

    fino al punto 4

---
+++ email-bonura/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Email } from '../Email/email.models';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, JsonPipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  emailvett:Email[] =[];
+  invia(email : HTMLInputElement, oggetto : HTMLInputElement, testo : HTMLInputElement): boolean{
+    console.log(email.value, oggetto.value, testo.value)
+    this.emailvett.push(new Email (email.value, oggetto.value, testo.value));
+    return false;
+  }
+}
---
+++ email-bonura/src/app/sent-mail/sent-mail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sent-mail',
+  standalone: true,
+  imports: [],
+  templateUrl: './sent-mail.component.html',
+  styleUrl: './sent-mail.component.css'
+})
+export class SentMailComponent {
+
+}
