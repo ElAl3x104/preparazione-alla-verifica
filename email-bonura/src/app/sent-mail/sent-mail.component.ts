0dd9b7feae79afeb0bc96d9e0cfc569afb745c78
Bonura_Alex <162216566+ElAl3x104@users.noreply.github.com>
2025-04-11 09:00:31 +0000

    verifica quasi completa

--- email-bonura/src/app/sent-mail/sent-mail.component.ts
+++ email-bonura/src/app/sent-mail/sent-mail.component.ts
@@ -1,14 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Email } from '../../Email/email.models';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-sent-mail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sent-mail.component.html',
   styleUrl: './sent-mail.component.css'
 })
 export class SentMailComponent {
   @Input() email!: Email;
+  visibile = false;
+  labelColor: string = 'white'
+  espandi(): boolean {
+    this.visibile = !this.visibile;
+    return false;
+  }
+  speciale(): void {
+    if (this.labelColor == 'yellow')
+      this.labelColor = 'white'
+    else
+      this.labelColor = 'yellow'
+  }
 }
